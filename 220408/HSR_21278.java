407dd205fef4f468e3924b7852833234593f0064
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2022-04-11 09:29:38 +0900

    HSR

--- 220408/HSR_21278.java
+++ 220408/HSR_21278.java
@@ -20,16 +20,14 @@ public class HSR_21278 {
 				map[i][j]=INF;
 			}
 		}
-		
-		// 간선 정보 입력 받기
+	
 		for(int i=0;i<m;i++) {
 			int a=sc.nextInt();
 			int b=sc.nextInt();
 			
 			map[a][b]=map[b][a]=1;
 		}
-		
-		// 플로이드 와샬 알고리즘
+
 		for(int k=1;k<=n;k++) 
 			for(int i=1;i<=n;i++)
 				for(int j=1;j<=n;j++)
@@ -41,9 +39,9 @@ public class HSR_21278 {
 		
 		for(int i=1;i<=n;i++) {
 			for(int j=i+1;j<=n;j++) {
-				// 2개의 지점을 치킨집으로 선정
+				
 				int dis=distance(i,j);
-				// 더 작은 값을 찾으면 치킨집 장소와 최솟값 갱신
+				
 				if(min>dis) {
 					point1=i;
 					point2=j;
@@ -52,14 +50,11 @@ public class HSR_21278 {
 			}
 		}
 		
-		// 왕복 거리이기 때문에 min*2 한 값을 출력
+		
 		System.out.println(point1+" "+point2+" "+min*2);
 	}
 	
-	/*
-	 * 모든 건물에서 가장 가까운 치킨집까지 왕복하는 최단 시간의 총합을 구하는 문제이기 때문에
-	 * 두 치킨집 중 더 가까운 치킨집까지의 거리를 구해 return 한다. 
-	 */
+
 	static int distance(int x, int y) {
 		int result=0;
 		for(int i=1;i<=n;i++)
