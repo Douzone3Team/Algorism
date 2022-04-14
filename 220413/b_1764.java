3c47f4b07fea71ee2226fa0c7f2c3a252e6db8f3
chaerin99 <81277321+chaerin99@users.noreply.github.com>
2022-04-14 09:13:53 +0900

    UCR

---
+++ 220413/b_1764.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class b_1764 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashSet<String> hs = new HashSet<String>();
+		ArrayList<String> list = new ArrayList<String>();
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			hs.add(st.nextToken());
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			if(hs.contains(str)) {
+				list.add(str);
+			}
+		}
+		
+		System.out.println(list.size());
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+}
+
