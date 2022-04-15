6f81b181e331134b15b229b5d1b7ac03c4b262e1
2jelee <82863823+2jelee@users.noreply.github.com>
2022-04-15 16:29:56 +0900

    LJE

--- 220414/JEL_3986.java
+++ 220414/JEL_3986.java
@@ -5,23 +5,23 @@ public class JEL_3986 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int cnt = 0;
+		int count = 0;
 		
     for(int i = 0; i < n; i++) {
 			String line = br.readLine();
 			Stack<Character> stack = new Stack<>();
 			
-			for(int j=0; j<line.length(); j++) {
-				if(stack.size()>0 && stack.peek() == line.charAt(j)) {
+			for(int j = 0; j < line.length(); j++) {
+				if(stack.size() > 0 && stack.peek() == line.charAt(j)) {
 					stack.pop();
 				} else {
 					stack.push(line.charAt(j));
 				}
 			}
 			if(stack.size() == 0) {
-        cnt++;
+        count++;
       }
     }
-		System.out.println(cnt);
+		System.out.println(count);
 	}
 }
