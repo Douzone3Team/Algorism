5ad8bed3c362b91a16fae3b6621748279cf4b4dc
gktjdfhr2 <74586766+gktjdfhr2@users.noreply.github.com>
2022-04-28 17:19:49 +0900

    Update HSR_14889.java

--- 220427/HSR_14889.java
+++ 220427/HSR_14889.java
@@ -13,7 +13,6 @@ public class HSR_14889 {
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         isvisit = new boolean[N];
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < N; j++) {
@@ -28,13 +27,11 @@ public class HSR_14889 {
         // 팀 조합이 완성될 경우
         if(count == N / 2) {
 			/*
-			 방문한 팀과 방문하지 않은 팀을 각각 나누어
-			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
+			 방문한 팀과 방문하지 않은 팀을 각각 나누어 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
 			*/
             diff();
             return;
         }
-
         for(int i = idx; i < N; i++) {
             // 방문하지 않았다면?
             if(!isvisit[i]) {
@@ -44,12 +41,10 @@ public class HSR_14889 {
             }
         }
     }
-
     // 두 팀의 능력치 차이를 계산하는 함수
     static void diff() {
         int team_start = 0;
         int team_link = 0;
-
         for (int i = 0; i < N - 1; i++) {
             for (int j = i + 1; j < N; j++) {
                 // i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스
@@ -66,7 +61,6 @@ public class HSR_14889 {
         }
         // 두 팀의 점수 차이 (절댓값)
         int val = Math.abs(team_start - team_link);
-
 		/*
 		  두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
 		  더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
@@ -75,9 +69,6 @@ public class HSR_14889 {
             System.out.println(val);
             System.exit(0);
         }
-
         Min = Math.min(val, Min);
-
-
     }
 }
