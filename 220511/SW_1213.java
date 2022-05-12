9fb376fb48aec58fd4c22cfbc87579479a3e1c65
2jelee <82863823+2jelee@users.noreply.github.com>
2022-05-12 11:38:41 +0900

    LJE

---
+++ 220511/SW_1213.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SW_1213 { 
+	static String test, m;
+    static Scanner sc = new Scanner(System.in);
+    
+    static int getSubNum() {
+        int answer = 0;
+        int ml = m.length();
+        for(int i = 0; i <= test.length()-ml; i++) {
+            if(test.substring(i, i+ml).equals(m)) { answer++; }
+        }
+        return answer;
+    }
+    static void solv() {
+        for(int i=1; i<=10; i++) {
+            sc.nextInt(); m = sc.next(); test = sc.next();
+            System.out.println("#"+i+" "+getSubNum());
+        }
+    }
+ 
+    public static void main(String[] args) { 
+    	solv();
+    } 
+} 
